43b2b61d7362924db1475d89ba2d654a74d3151d
麦子 <woshiyouxiangyo@gmail.com>
2015-11-23 23:54:00 -0800

    add Badu Network

---
+++ Demos/baduwangluo/js/mx.js
@@ -0,0 +1,16 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    // direction: 'vertical',
+    loop: true,
+    autoplay : 1000,
+    paginationClickable: true,
+
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+
+    // 如果需要前进后退按钮
+    nextButton: '.next',
+    prevButton: '.prev',
+
+    // 如果需要滚动条
+    // scrollbar: '.swiper-scrollbar',
+  })
